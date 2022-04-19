76951bc63cb367530f084dc8184a2b68b137f15c
loganzimm4 <101672520+loganzimm4@users.noreply.github.com>
2022-04-19 09:37:37 -0400

    add div content in js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 fetch("https://owen-wilson-wow-api.herokuapp.com/wows/random?results=5")
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => {
+    data.forEach(arr => {
+        displayMovie(arr)
+    });
+})
 
-console.log(1+1)
+
+
+// Render Functions
+
+const displayMovie = (arr) => {
+    console.log(arr)
+   let div = document.querySelector('#top-list')
+   let span = document.createElement('span')
+   span.textContent = arr.movie
+   div.append(span)
+}
