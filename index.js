9d0347daa60b929e93abf6b38a799ec6e41b0864
Louis Y <louisy603@hotmail.com>
2022-04-19 12:02:55 -0400

    added two js lines and moved some styling around

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//FETCHES
+//Movie info on load
+fetch("https://owen-wilson-wow-api.herokuapp.com/wows/ordered/29")
+.then(res => res.json())
+.then(arr => onLoadMovie(arr))
+
+
 fetch("https://owen-wilson-wow-api.herokuapp.com/wows/random?results=5")
 .then(res => res.json())
 .then(data => {
@@ -6,9 +13,10 @@ fetch("https://owen-wilson-wow-api.herokuapp.com/wows/random?results=5")
     });
 })
 
+
 //Variable declarations
 let poster = document.querySelector('#poster')
-let name = document.querySelector('#name')
+let movieName = document.querySelector('#name')
 let yr = document.querySelector('#year')
 let char = document.querySelector('#character')
 let quote = document.querySelector('#quote')
@@ -16,7 +24,6 @@ let video = document.querySelector('#video')
 let audio = document.querySelector('#audio')
 
 // Render Functions
-
 const displayMovie = (arr) => {
     //console.log(arr)
    let div = document.querySelector('#top-list')
@@ -29,35 +36,28 @@ const displayMovie = (arr) => {
    img.addEventListener('click', (e) => handleClick(e, arr))
 }
 
+function onLoadMovie(arr) {
+    poster.src = arr.poster
+    movieName.textContent = arr.movie
+    yr.textContent = arr.year
+    char.textContent = arr.character
+    quote.textContent = `Full Quote: "${arr.full_line}"`
+    video.src = arr.video['1080p']
+    audio.src = arr.audio
+}
+
+
+
+//CALLBACKS
 const handleClick = (e, arr) => {
     //console.log(arr)
     poster.src = arr.poster
-    name.textContent = arr.movie
+    movieName.textContent = arr.movie
     yr.textContent = arr.year
     char.textContent = arr.character
     quote.textContent = `Full Quote: "${arr.full_line}"`
+    video.src = arr.video['1080p']
+    audio.src = arr.audio
 }
 
 
-//Movie info on load
-fetch("https://owen-wilson-wow-api.herokuapp.com/wows/ordered/29")
-.then(res => res.json())
-.then(arr => onLoadMovie(arr))
-
-function onLoadMovie(arr) {
-    poster.src = arr.poster
-    name.textContent = arr.movie
-    yr.textContent = arr.year
-    char.textContent = arr.character
-    quote.textContent = `Full Quote: "${arr.full_line}"`
-
-    video.src = arr.video['1080p']
-    audio.src = arr.audio
-    console.log(arr.video['1080p'])
-
-    console.log(video)
-    console.log(audio)
-    video.src = arr.video['1080p']
-    audio.src = arr.audio
-
-}
